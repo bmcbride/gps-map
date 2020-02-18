3c7386d5378962e2b2e84f2e7728aa68a1a9ff07
Bryan McBride <mcbride.bryan@gmail.com>
2020-02-17 19:08:07 -0500

    initial code commit

---
+++ assets/js/app.js
@@ -0,0 +1,367 @@
+const map = L.map("map", {
+  zoomSnap: 0,
+  maxZoom: 22,
+  zoomControl: false
+}).fitWorld();
+map.attributionControl.setPrefix(`<a href="#" onclick="showHelp(); return false;">Help</a>`);
+
+map.once("locationfound", function(e) {
+  map.fitBounds(e.bounds, {maxZoom: 18});
+});
+
+const layers = {
+  basemaps: {
+    "Streets": L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.@2xpng", {
+      maxNativeZoom: 18,
+      maxZoom: map.getMaxZoom(),
+      attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attribution">CARTO</a>',
+    }).addTo(map),
+
+    "Aerial": L.tileLayer.wms("https://orthos.dhses.ny.gov/ArcGIS/services/Latest/MapServer/WMSServer", {
+      layers: "0,1,2,3,4",
+      format: "image/png",
+      transparent: true,
+      maxNativeZoom: 18,
+      maxZoom: map.getMaxZoom(),
+      attribution: "<a href='https://gis.ny.gov/gateway/mg/webserv/webserv.html' class='external'>NYSDOP</a>"
+    }),
+    
+    "Topo": L.tileLayer("https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}", {
+      maxNativeZoom: 16,
+      maxZoom: map.getMaxZoom(),
+      attribution: "USGS",
+    }),
+
+    "Charts": L.tileLayer("https://tileservice.charts.noaa.gov/tiles/50000_1/{z}/{x}/{y}.png", {
+      maxNativeZoom: 18,
+      maxZoom: map.getMaxZoom(),
+      attribution: "NOAA",
+    }),
+
+    "None": L.tileLayer("", {
+      maxZoom: map.getMaxZoom()
+    })
+  },
+
+  select: L.featureGroup(null).addTo(map),
+  overlays: {}
+};
+
+/*** Begin custom input control for adding local file ***/
+L.Control.AddFile = L.Control.extend({
+  onAdd: function(map) {
+    fileInput = L.DomUtil.create("input", "hidden");
+    fileInput.type = "file";
+    fileInput.accept = ".mbtiles, .geojson, .kml, .gpx";
+    fileInput.style.display = "none";
+    
+    fileInput.addEventListener("change", function () {
+      const file = fileInput.files[0];
+      handleFile(file);
+      this.value = "";
+    }, false);
+    
+    const div = L.DomUtil.create("div", "leaflet-bar leaflet-control");
+    div.innerHTML = `
+      <a class='leaflet-bar-part leaflet-bar-part-single file-control-btn' title='Load File' onclick='fileInput.click();'>
+        <i id='loading-icon' class='fas fa-map-marked-alt'></i>
+      </a>
+    `;
+    return div
+  }
+});
+
+L.control.addfile = function(opts) {
+  return new L.Control.AddFile(opts);
+}
+/*** End custom control ***/
+
+const controls = {
+  layerCtrl: L.control.layers(layers.basemaps, null, {
+    collapsed: true,
+    position: "topright"
+  }).addTo(map),
+
+  locateCtrl: L.control.locate({
+    icon: "fas fa-crosshairs",
+    setView: "untilPan",
+    cacheLocation: true,
+    position: "topleft",
+    flyTo: false,
+    keepCurrentZoomLevel: true,
+    circleStyle: {
+      interactive: false
+    },
+    markerStyle: {
+      interactive: false
+    },
+    locateOptions: {
+      enableHighAccuracy: true/*,
+      maxZoom: 19*/
+    },
+    onLocationError: function(e) {
+      alert(e.message);
+    }
+  }).addTo(map),
+
+  fileCtrl: L.control.addfile({
+    position: "topleft"
+  }).addTo(map)
+};
+
+function handleFile(file) {
+  showLoader();
+
+  const name = file.name.split(".").slice(0, -1).join(".");
+
+  if (file.name.endsWith(".mbtiles")) {
+    loadRaster(file, name);
+  } else if (file.name.endsWith(".geojson") || file.name.endsWith(".kml") || file.name.endsWith(".gpx")) {
+    const format = file.name.split(".").pop();
+    loadVector(file, name, format);
+  } else {
+    alert("MBTiles, GeoJSON, KML, and GPX files supported.");
+    hideLoader();
+  }
+}
+
+function loadVector(file, name, format) {
+  const reader = new FileReader();
+  let geojson = null;
+
+  reader.onload = function(e) {
+    if (format == "geojson") {
+      geojson = JSON.parse(reader.result);
+    } else if (format == "kml") {
+      const kml = (new DOMParser()).parseFromString(reader.result, "text/xml");
+      geojson = toGeoJSON.kml(kml, {styles: true});
+    } else if (format == "gpx") {
+      const gpx = (new DOMParser()).parseFromString(reader.result, "text/xml");
+      geojson = toGeoJSON.gpx(gpx);
+    }
+
+    const layer = L.geoJSON(geojson, {
+      renderer: L.canvas({
+        padding: 0.5,
+        tolerance: 10
+      }),
+      style: function (feature) {
+        return {
+          color: feature.properties["stroke"] ? feature.properties["stroke"] : "#3388ff",
+          opacity: feature.properties["stroke-opacity"] ? feature.properties["stroke-opacity"] : 1.0,
+          weight: feature.properties["stroke-width"] ? feature.properties["stroke-width"] : 3,
+          fillColor: feature.properties["fill"] ? feature.properties["fill"] : "#3388ff",
+          fillOpacity: feature.properties["fill-opacity"] ? feature.properties["fill-opacity"] : 0.2,
+        };
+      },
+      pointToLayer: function (feature, latlng) {
+        if (format == "kml") {
+          return L.circleMarker(latlng, {
+            radius: 6
+          }); 
+        } else {
+          return L.marker(latlng);
+        }
+      },
+      onEachFeature: function (feature, layer) {
+        let table = "<div style='overflow:auto;'><table>";
+        
+        if (feature && feature.geometry) {
+          if (feature.geometry.type.includes("LineString")) {
+            const length = turf.length(layer.toGeoJSON(), {units: "miles"});
+            table += `<tr><th>LENGTH</th><td>${length.toFixed(1)} Miles</td></tr>`;
+          } else if (feature.geometry.type === "Polygon") {
+            const sqMeters = turf.area(layer.toGeoJSON());
+            const acres = (sqMeters / 4046.86);
+            const area = (acres < 640) ? (acres.toFixed(1) + " Acres") : ((sqMeters / 2589990).toFixed(1) + " Sq. Miles");
+            table += `<tr><th>AREA</th><td>${area}</td></tr>`;
+          }
+        }
+
+        const styleProps = ["styleUrl", "styleHash", "styleMapHash", "stroke", "stroke-opacity", "stroke-width", "opacity", "fill", "fill-opacity", "icon", "scale"];
+        for (const key in feature.properties) {
+          if (feature.properties.hasOwnProperty(key) && styleProps.indexOf(key) == -1) {
+            table += "<tr><th>" + key.toUpperCase() + "</th><td>" + formatProperty(feature.properties[key]) + "</td></tr>";
+          }
+        }
+        table += "</table></div>";
+        layer.bindPopup(table, {
+          maxHeight: 300,
+          maxWidth: 250
+        });
+        layer.on({
+          popupclose: function (e) {
+            layers.select.clearLayers();
+          },
+          click: function (e) {
+            layers.select.clearLayers();
+            layers.select.addLayer(L.geoJSON(layer.toGeoJSON(), {
+              style: {
+                color: "#00FFFF",
+                weight: 5
+              },
+              pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+                  radius: 6,
+                  color: "#00FFFF"
+                }); 
+              }
+            }))
+          }
+        });
+      }
+      
+    }).addTo(map);
+
+    addLayer(layer, name);
+    layers.overlays[L.Util.stamp(layer)] = layer;
+    zoomToLayer(L.Util.stamp(layer));
+  }
+
+  reader.readAsText(file);
+}
+
+function loadRaster(file, name) {
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    const layer = L.tileLayer.mbTiles(reader.result, {
+      autoScale: true,
+      fitBounds: true,
+      updateWhenIdle: false
+    }).on("databaseloaded", function(e) {
+      name = (layer.options.name ? layer.options.name : name);
+      addLayer(layer, name);
+    }).addTo(map);
+    layers.overlays[L.Util.stamp(layer)] = layer;
+  }
+
+  reader.readAsArrayBuffer(file);
+}
+
+function addLayer(layer, name) {
+  hideLoader();
+  controls.layerCtrl.addOverlay(layer, `
+    <span class="layer-name" id="${L.Util.stamp(layer)}">
+      ${name}
+    </span>
+    <span class="layer-buttons">
+      <span style="display: ${layer instanceof L.GeoJSON ? 'none' : 'unset'}">
+        <a class="layer-btn" href="#" title="Change opacity" onclick="changeOpacity(${L.Util.stamp(layer)}); return false;"><i class="fas fa-adjust"></i></a>
+      </span>
+      <a class="layer-btn" href="#" title="Zoom to layer" onclick="zoomToLayer(${L.Util.stamp(layer)}); return false;"><i class="fas fa-expand-arrows-alt"></i></a>
+      <a class="layer-btn" href="#" title="Remove layer" onclick="removeLayer(${L.Util.stamp(layer)}, '${name}'); return false;"><i class="fas fa-trash" style="color: red"></i></a>
+    </span>
+    <div style="clear: both;"></div>
+  `);
+}
+
+function zoomToLayer(id) {
+  const layer = layers.overlays[id];
+  if (!map.hasLayer(layer)) {
+    map.addLayer(layers.overlays[id]);
+  }
+  if (layer.options.bounds) {
+    map.fitBounds(layer.options.bounds);
+  }
+  else {
+    map.fitBounds(layer.getBounds());
+  }
+}
+
+function removeLayer(id, name) {
+  const cfm = confirm(`Remove ${name}?`);
+  if (cfm == true) {
+    const layer = layers.overlays[id];
+    if (!map.hasLayer(layer)) {
+      map.addLayer(layers.overlays[id]);
+    }
+    map.removeLayer(layer);
+    controls.layerCtrl.removeLayer(layer);
+  }
+}
+
+function changeOpacity(id) {
+  const layer = layers.overlays[id];
+  if (!map.hasLayer(layer)) {
+    map.addLayer(layers.overlays[id]);
+  }
+  let value = layer.options.opacity;
+  if (value > 0.2) {
+    layer.setOpacity((value-0.2).toFixed(1));  
+  } else {
+    layer.setOpacity(1);
+  }
+}
+
+function formatProperty(value) {
+  if (typeof value == "string" && (value.indexOf("http") === 0 || value.indexOf("https") === 0)) {
+    return `<a href="${value}" target="_blank">${value}</a>`;
+  } else {
+    return value;
+  }
+}
+
+function showLoader() {
+  const loadingIcon = document.getElementById("loading-icon");
+  loadingIcon.classList.remove("fa-map-marked-alt");
+  loadingIcon.classList.add("fa-spinner", "fa-spin");
+}
+
+function hideLoader() {
+  const loadingIcon = document.getElementById("loading-icon");
+  loadingIcon.classList.remove("fa-spin", "fa-spinner");
+  loadingIcon.classList.add("fa-map-marked-alt");
+}
+
+function goOffline() {
+  const basemaps = Object.keys(layers.basemaps);
+  for (const layer of basemaps) {
+    if (layer == "None") {
+      map.addLayer(layers.basemaps[layer]);
+    } else {
+      map.removeLayer(layers.basemaps[layer]);
+    }
+  }
+}
+
+function showHelp() {
+  const info = 'Welcome to GPS Map, an offline capable map viewer with GPS integration!\n\nTap the crosshairs button to locate, zoom to, and follow your GPS location.\n\nTap the map/marker button to load an MBTiles, GeoJSON, KML, or GPX file directly from your device.\n\nTap the layers button to view online basemaps and manage offline layers.';
+  alert(info);
+}
+
+window.addEventListener("offline",  function(e) {
+  goOffline();
+});
+
+const dropArea = document.getElementById("map");
+
+["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+  dropArea.addEventListener(eventName, function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }, false);
+});
+
+["dragenter", "dragover"].forEach(eventName => {
+  dropArea.addEventListener(eventName, showLoader, false);
+});
+
+["dragleave", "drop"].forEach(eventName => {
+  dropArea.addEventListener(eventName, hideLoader, false);
+});
+
+dropArea.addEventListener("drop", function(e) {
+  const file = e.dataTransfer.files[0];
+  handleFile(file);
+}, false);
+
+initSqlJs({
+  locateFile: function() {
+    return "assets/vendor/sqljs-1.1.0/sql-wasm.wasm";
+  }
+}).then(function(SQL){
+  navigator.onLine ? null : goOffline();
+});
+
+controls.locateCtrl.start();
---
+++ service-worker.js
@@ -0,0 +1,99 @@
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the 'License');
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an 'AS IS' BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+const PRECACHE = 'precache-02.17.20.8';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  'manifest.json',
+  'assets/img/apple-touch-icon.png',
+  'assets/img/favicon-32x32.png',
+  'assets/img/favicon-16x16.png',
+  'assets/img/favicon-512x512.png',
+  'assets/img/android-chrome-192x192.png',
+  'assets/vendor/leaflet-1.6.0/images/layers.png',
+  'assets/vendor/leaflet-1.6.0/images/layers-2x.png',
+  'assets/vendor/leaflet-1.6.0/images/marker-icon.png',
+  'assets/vendor/leaflet-1.6.0/images/marker-icon-2x.png',
+  'assets/vendor/leaflet-1.6.0/images/marker-shadow.png',
+  'assets/vendor/leaflet-1.6.0/leaflet.css',
+  'assets/vendor/leaflet-1.6.0/leaflet.js',
+  'assets/vendor/leaflet-locatecontrol-0.70.0/L.Control.Locate.min.css',
+  'assets/vendor/leaflet-locatecontrol-0.70.0/L.Control.Locate.min.js',
+  'assets/vendor/leaflet-mbtiles/Leaflet.TileLayer.MBTiles.js',
+  'assets/vendor/togeojson/togeojson.js',
+  'assets/vendor/turf-5.1.6/turf-area-length.min.js',
+  'assets/vendor/fontawesome-free-5.12.1-web/css/all.min.css',
+  'assets/vendor/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.ttf',
+  'assets/vendor/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.woff2',
+  'assets/vendor/sqljs-1.1.0/sql-wasm.js',
+  'assets/vendor/sqljs-1.1.0/sql-wasm.wasm',
+  'assets/css/app.css',
+  'assets/js/app.js'
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE, 'map-cache')
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        /* Disable runtime caching
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+        */
+
+        return fetch(event.request).then(response => {
+          return response;
+        });
+      })
+    );
+  }
+});
