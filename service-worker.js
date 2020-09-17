3456ba4b60efb951f3482ab12448da6cbffca8d9
Bryan McBride <mcbride.bryan@gmail.com>
2020-09-17 13:01:41 -0400

    revert to leaflet 1.6.0 to fix Safari issues
    See https://github.com/Leaflet/Leaflet/issues/7255

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '09.15.20.1'},
-  {url: './', revision: '09.15.20.1'}, // Alias for index.html
+  {url: 'index.html', revision: '09.17.20.1'},
+  {url: './', revision: '09.17.20.1'}, // Alias for index.html
   {url: 'manifest.json', revision: '09.09.20.1'},
   {url: 'assets/img/apple-touch-icon.png', revision: '09.09.20.1'},
   {url: 'assets/img/favicon-32x32.png', revision: '09.09.20.1'},
@@ -19,13 +19,13 @@ workbox.precaching.precacheAndRoute([
   {url: 'assets/vendor/vex-4.1.0/css/vex-theme-top.css', revision: '09.09.11.1'},
   {url: 'assets/vendor/sqljs-1.3.0/sql-wasm.js', revision: '09.09.20.1'},
   {url: 'assets/vendor/sqljs-1.3.0/sql-wasm.wasm', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/images/layers.png', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/images/layers-2x.png', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/images/marker-icon.png', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/images/marker-icon-2x.png', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/images/marker-shadow.png', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/leaflet.css', revision: '09.09.20.1'},
-  {url: 'assets/vendor/leaflet-1.7.1/leaflet.js', revision: '09.09.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/images/layers.png', revision: '09.17.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/images/layers-2x.png', revision: '09.17.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/images/marker-icon.png', revision: '09.17.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/images/marker-icon-2x.png', revision: '09.17.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/images/marker-shadow.png', revision: '09.17.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/leaflet.css', revision: '09.17.20.1'},
+  {url: 'assets/vendor/leaflet-1.6.0/leaflet.js', revision: '09.17.20.1'},
   {url: 'assets/vendor/leaflet-locatecontrol-0.72.1/L.Control.Locate.min.js', revision: '09.09.20.1'},
   {url: 'assets/vendor/leaflet-mbtiles/Leaflet.TileLayer.MBTiles.js', revision: '09.09.20.1'},
   {url: 'assets/vendor/togeojson-0.16.0/togeojson.js', revision: '09.09.20.1'},
