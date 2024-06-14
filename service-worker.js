97f2cb93ac1eb5dfe35a935c5fb59ae7a2e46ddb
Bryan McBride <mcbride.bryan@gmail.com>
2024-06-14 18:38:21 -0400

    Bump service worker

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '05.08.24.2'},
+  {url: 'index.html', revision: '06.14.24.1'},
   {url: 'manifest.json', revision: '11.15.23.1'},
   {url: 'assets/js/app.js', revision: '06.14.24.2'},
   {url: 'assets/css/app.css', revision: '11.15.23.1'},
