8a9ad65b09d76c733290ab678ad06df3f61f6cc8
Bryan McBride <mcbride.bryan@gmail.com>
2024-10-28 13:03:30 -0400

    updgrade to pmtiles-3.2.0 and sweetalert2-11.14.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const app = {
-  version: "2024.08.16.1"
+  version: "2024.10.28.1"
 }
 
 const mapStore = localforage.createInstance({
--- assets/vendor/pmtiles-3.2.0/pmtiles.js
+++ assets/vendor/pmtiles-3.2.0/pmtiles.js
@@ -57,6 +57,7 @@ var pmtiles = (() => {
     leafletRasterLayer: () => leafletRasterLayer,
     readVarint: () => readVarint,
     tileIdToZxy: () => tileIdToZxy,
+    tileTypeExt: () => tileTypeExt,
     zxyToTileId: () => zxyToTileId
   });
 
@@ -839,7 +840,14 @@ var pmtiles = (() => {
     return { cancel: () => abortController.abort() };
   };
   var Protocol = class {
-    constructor() {
+    /**
+     * Initialize the MapLibre PMTiles protocol.
+     *
+     * * metadata: also load the metadata section of the PMTiles. required for some "inspect" functionality
+     * and to automatically populate the map attribution. Requires an extra HTTP request.
+     */
+    constructor(options) {
+      /** @hidden */
       this.tilev4 = (params, abortController) => __async(this, null, function* () {
         if (params.type === "json") {
           const pmtilesUrl2 = params.url.substr(10);
@@ -847,6 +855,11 @@ var pmtiles = (() => {
           if (!instance2) {
             instance2 = new PMTiles(pmtilesUrl2);
             this.tiles.set(pmtilesUrl2, instance2);
+          }
+          if (this.metadata) {
+            return {
+              data: yield instance2.getTileJson(params.url)
+            };
           }
           const h = yield instance2.getHeader();
           return {
@@ -888,10 +901,20 @@ var pmtiles = (() => {
       });
       this.tile = v3compat(this.tilev4);
       this.tiles = /* @__PURE__ */ new Map();
+      this.metadata = (options == null ? void 0 : options.metadata) || false;
     }
+    /**
+     * Add a {@link PMTiles} instance to the global protocol instance.
+     *
+     * For remote fetch sources, references in MapLibre styles like pmtiles://http://...
+     * will resolve to the same instance if the URLs match.
+     */
     add(p) {
       this.tiles.set(p.source.getKey(), p);
     }
+    /**
+     * Fetch a {@link PMTiles} instance by URL, for remote PMTiles instances.
+     */
     get(url) {
       return this.tiles.get(url);
     }
@@ -1083,6 +1106,19 @@ var pmtiles = (() => {
     TileType2[TileType2["Avif"] = 5] = "Avif";
     return TileType2;
   })(TileType || {});
+  function tileTypeExt(t) {
+    if (t === 1 /* Mvt */)
+      return ".mvt";
+    if (t === 2 /* Png */)
+      return ".png";
+    if (t === 3 /* Jpeg */)
+      return ".jpg";
+    if (t === 4 /* Webp */)
+      return ".webp";
+    if (t === 5 /* Avif */)
+      return ".avif";
+    return "";
+  }
   var HEADER_SIZE_BYTES = 127;
   function findTile(entries, tileId) {
     let m = 0;
@@ -1128,10 +1164,23 @@ var pmtiles = (() => {
       this.url = url;
       this.customHeaders = customHeaders;
       this.mustReload = false;
+      let userAgent = "";
+      if ("navigator" in globalThis) {
+        userAgent = globalThis.navigator.userAgent || "";
+      }
+      const isWindows = userAgent.indexOf("Windows") > -1;
+      const isChromiumBased = /Chrome|Chromium|Edg|OPR|Brave/.test(userAgent);
+      this.chromeWindowsNoCache = false;
+      if (isWindows && isChromiumBased) {
+        this.chromeWindowsNoCache = true;
+      }
     }
     getKey() {
       return this.url;
     }
+    /**
+     * Mutate the custom [Headers](https://developer.mozilla.org/en-US/docs/Web/API/Headers) set for all requests to the remote archive.
+     */
     setHeaders(customHeaders) {
       this.customHeaders = customHeaders;
     }
@@ -1150,6 +1199,8 @@ var pmtiles = (() => {
         let cache;
         if (this.mustReload) {
           cache = "reload";
+        } else if (this.chromeWindowsNoCache) {
+          cache = "no-store";
         }
         let resp = yield fetch(this.url, {
           signal,
@@ -1602,7 +1653,7 @@ var pmtiles = (() => {
       });
     }
     /**
-     * Primary method to get a single tile bytes from an archive.
+     * Primary method to get a single tile's bytes from an archive.
      *
      * Returns undefined if the tile does not exist in the archive.
      */
@@ -1651,6 +1702,35 @@ var pmtiles = (() => {
           }
           throw e;
         }
+      });
+    }
+    /**
+     * Construct a [TileJSON](https://github.com/mapbox/tilejson-spec) object.
+     *
+     * baseTilesUrl is the desired tiles URL, excluding the suffix `/{z}/{x}/{y}.{ext}`.
+     * For example, if the desired URL is `http://example.com/tileset/{z}/{x}/{y}.mvt`,
+     * the baseTilesUrl should be `https://example.com/tileset`.
+     */
+    getTileJson(baseTilesUrl) {
+      return __async(this, null, function* () {
+        const header = yield this.getHeader();
+        const metadata = yield this.getMetadata();
+        const ext = tileTypeExt(header.tileType);
+        return {
+          tilejson: "3.0.0",
+          scheme: "xyz",
+          tiles: [`${baseTilesUrl}/{z}/{x}/{y}${ext}`],
+          // biome-ignore lint: TileJSON spec
+          vector_layers: metadata.vector_layers,
+          attribution: metadata.attribution,
+          description: metadata.description,
+          name: metadata.name,
+          version: metadata.version,
+          bounds: [header.minLon, header.minLat, header.maxLon, header.maxLat],
+          center: [header.centerLon, header.centerLat, header.centerZoom],
+          minzoom: header.minZoom,
+          maxzoom: header.maxZoom
+        };
       });
     }
   };
