7253c4daf6e7d5e0ae36b5258fbc1751f60d3e98
Bryan McBride <mcbride.bryan@gmail.com>
2026-07-23 15:23:32 -0400

    upgrade to pmtiles v4.4.0

--- service-worker.js
+++ service-worker.js
@@ -28,8 +28,8 @@ workbox.precaching.precacheAndRoute([
   {url: 'assets/vendor/leaflet-1.9.4/leaflet.js', revision: '11.15.23.1'},
   {url: 'assets/vendor/leaflet-locatecontrol-0.90.0/L.Control.Locate.min.css', revision: '07.23.26.1'},
   {url: 'assets/vendor/leaflet-locatecontrol-0.90.0/L.Control.Locate.min.js', revision: '07.23.26.1'},
-  {url: 'assets/vendor/pmtiles-4.4.0/pmtiles.js', revision: '02.19.26.1'},
-  {url: 'assets/vendor/pmtiles-4.4.0/pmtiles.js.map', revision: '02.19.26.1'},
+  {url: 'assets/vendor/pmtiles-4.4.1/pmtiles.js', revision: '07.23.26.1'},
+  {url: 'assets/vendor/pmtiles-4.4.1/pmtiles.js.map', revision: '07.23.26.1'},
   {url: 'assets/vendor/sweetalert2-11.26.19/sweetalert2.all.min.js', revision: '02.19.26.1'}
 ], {
   ignoreURLParametersMatching: [/.*/]
